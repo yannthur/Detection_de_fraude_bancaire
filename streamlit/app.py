e7069c5f816de71adc5ebb41e970b090527a15da
yannthur <nzognioyann@gmail.com>
2025-06-05 17:32:35 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -25,7 +25,8 @@ def BoxCoxTransformation(X):
 
 @st.cache_data
 def load_data():
-    data = pd.read_csv("../data/data.csv")
+    file_path = os.path.join(os.path.dirname(__file__), "../data/data.csv")
+    data = pd.read_csv(file_path)
     return data
 
 def LabelEncoder_(data):
